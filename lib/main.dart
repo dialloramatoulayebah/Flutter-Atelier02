a01d865d72e5c14aae1a17bc149b631914850d61
dialloramatoulayebah <129418622+dialloramatoulayebah@users.noreply.github.com>
2025-11-05 15:15:32 -0500

    Etape 3: Container

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,45 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
           child: Column(
-            children: [Text('Hello, World!'), Text("C'est l'Atelier 02")],
+            children: [
+              Text('Hello, World!'),
+              Text("C'est l'Atelier 02"),
+              Container(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.purple,
+                  borderRadius: BorderRadius.all(Radius.elliptical(10, 5)),
+                ),
+                child: Text(
+                  'Boite stylisée avec Container',
+                  style: TextStyle(color: Colors.grey, fontSize: 18),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(20),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(255, 116, 180, 220),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Text(
+                  'Boite stylisée avec Container',
+                  style: TextStyle(color: Colors.grey, fontSize: 18),
+                ),
+              ),
+            ],
           ),
         ),
       ),
